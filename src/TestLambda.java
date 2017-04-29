2fbbd896aca25934d3e29cb5b5455b9ef8b067d1
sun-hongwei <1012191814@qq.com>
2017-04-29 23:40:25 +0800

    init

---
+++ src/TestLambda.java
@@ -0,0 +1,15 @@
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.junit.Test;
+
+import java.beans.Transient;
+
+/**
+ * Created by fan on 2017/4/29.
+ */
+public class TestLambda {
+
+    @Test
+    public void test1(){
+
+    }
+}
