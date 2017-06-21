4e5a01ba560b5e2133beb2403443e1c0e030f843
sun-hongwei <1012191814@qq.com>
2017-06-21 22:08:27 +0800

    add

---
+++ src/ThreadTest/ThreadTest4.java
@@ -0,0 +1,11 @@
+package ThreadTest;
+
+/**
+ * Created by sun on 2017/6/5.
+ */
+public class ThreadTest4 {
+
+    public static void main(String[] args) {
+        System.out.println(100.1*3);
+    }
+}
